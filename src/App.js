42019026f05f3e3c31ee2812fef5d619c422b5b6
Joshua Miles <josh.miles@dozuki.com>
2022-08-07 12:11:31 -0500

    -

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 style={{ fontWeight: 'bold', fontFamily: 'fantasy', fontSize: '2em', marginBottom: 0}}>
          The Cursed
-        </p>
+        </h1>
+        <a href="https://www.thecursed.games" style={{ color: 'white ', fontFamily: 'fantasy', opacity: 0.5}}>
+          https://www.thecursed.games
+        </a>
       </header>
     </div>
   );
